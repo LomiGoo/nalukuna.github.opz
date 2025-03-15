7bdd62cb33f56179d790b1eff7ceea4246fb125d
LomiGoo <chevellenacionales11@gmail.com>
2025-03-15 23:38:36 +0800

    Updated the project with new features

--- SchoolProjects/Java/PrcJrn_1/OOP/Encapsulation/src/rhsSecurity/src/guardInfo/guardInfo.java
+++ SchoolProjects/Java/PrcJrn_1/OOP/Encapsulation/src/rhsSecurity/src/guardInfo/guardInfo.java
@@ -1,20 +1,19 @@
 package guardInfo;
 
-public class guardInfo {
-										private static String usrN[] = new String[20];
-										private static String pass[] = new String[20];
-										private static String guardSh[] = new String[20];
-										
-										public String[] getUsrN() {
-																				return usrN;
-										}
-										
-										public String[] getPass() {
-																				return pass;
-										}
-										
-										public String[] getGuardSh() {
-																				return guardSh;
-										}
-}
+     public class guardInfo {
+          private static String usrN[] = new String[20];
+          private static String pass[] = new String[20];
+          private static String guardSh[] = new String[20];
 
+     public String[] getUsrN() {
+     return usrN;
+     }
+     
+     public String[] getPass() {
+     return pass;
+     }
+     
+     public String[] getGuardSh() {
+     return guardSh;
+     }
+}
